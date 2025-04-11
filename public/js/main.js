ab83d8f2fc09d3ce7f03b7027871db80bf6f9e5d
Alexandre <airtontunni@gmail.com>
2025-04-10 21:14:50 -0300

    continuando o trabalho em outra máquina

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,19 @@
+function addNumbers(event) {
+    event.preventDefault()
+    const firstNumber = Number(document.getElementById('firstNumber').value)
+    const secondNumber = Number(document.getElementById('secondNumber').value)
+    const inputSum = document.getElementById('result')
+    const boxShadow = document.getElementsByTagName('main')
+    if (!isNaN(firstNumber) && !isNaN(secondNumber)) {
+        const sum = firstNumber + secondNumber
+        inputSum.innerText = `Resultado ${sum}`
+        inputSum.classList.remove('hidden');
+        boxShadow.style.boxShadow = '2px 2px 4px rgb(34, 197, 94)'
+    } else {
+        inputSum.innerText = 'Por favor, insira dois números válidos.';
+        inputSum.classList.remove('hidden');
+    }
+}
 
-const firstNumber = document.getElementById('firstNumber')
-const secondNumber = document.getElementById('secondNumber')
+
 
