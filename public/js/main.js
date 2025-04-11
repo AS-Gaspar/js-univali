76111c668a44cce8011294c5f43a039bf67609cb
Alexandre <airtontunni@gmail.com>
2025-04-10 21:54:59 -0300

    por hoje chega

--- public/js/main.js
+++ public/js/main.js
@@ -3,12 +3,13 @@ function addNumbers(event) {
     const firstNumber = Number(document.getElementById('firstNumber').value)
     const secondNumber = Number(document.getElementById('secondNumber').value)
     const inputSum = document.getElementById('result')
-    const boxShadow = document.getElementsByTagName('main')
+    const container = document.getElementsByName('container')[0]
     if (!isNaN(firstNumber) && !isNaN(secondNumber)) {
         const sum = firstNumber + secondNumber
-        inputSum.innerText = `Resultado ${sum}`
+        const textResult = inputSum.appendChild('h3')
+        textResult.innerText = `Resultado ${sum}`
         inputSum.classList.remove('hidden');
-        boxShadow.style.boxShadow = '2px 2px 4px rgb(34, 197, 94)'
+        container.style.boxShadow = '2px 2px 10px rgb(34, 197, 94)'
     } else {
         inputSum.innerText = 'Por favor, insira dois números válidos.';
         inputSum.classList.remove('hidden');
